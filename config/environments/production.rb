b4d058de8b51b15134881159b4a7301d83caa664
dom <o.dubynyak@gmail.com>
2020-04-29 17:57:13 +0300

    drop db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
-
+  config.action_dispatch.x_sendfile_header = nil
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
